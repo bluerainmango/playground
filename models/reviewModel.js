47c20a56dee65f1a17c84e304b92588cad43b0db
Emily Yu <bluerainmango@gmail.com>
2020-06-10 22:40:57 -0700

    added review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,3 +1,28 @@
 const mongoose = require('mongoose');
 
-const reviewSchema = new mongoose.Schema({});
+const reviewSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'A review must belong to a user.'],
+  },
+  product: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Product',
+    required: [true, 'A review must belong to a product.'],
+  },
+  comment: {
+    type: String,
+    required: [true, 'A review must have comment.'],
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true,
+  },
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
